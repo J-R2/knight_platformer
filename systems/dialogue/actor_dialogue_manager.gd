54fcfdab191c6ba13014eba93fd4aaf7f0d68ad1
Jordan <104177490+J-R2@users.noreply.github.com>
2024-10-08 12:32:00 -0500

    dialogue.json _save() in correct format

--- systems/dialogue/actor_dialogue_manager.gd
+++ systems/dialogue/actor_dialogue_manager.gd
@@ -1,15 +1,46 @@
 extends Node
 
+@export var actor_name :String = "percival"
+#@export_file("*.json") var
+const DIALOGUE_FILE_PATH :String = "res://systems/dialogue/dialogue.json"
+
 var dialogue_items :Array[DialogueItem] = []
-#@export_file("*.json") 
-var DIALOGUE_FILE_PATH :String = "res://dialogue.csv"
+
+
 
 func _ready() -> void :
-	_load_dialogue_items()
+	#_load_dialogue_items()
+	#var dialogue_item = DialogueItem.new()
+	#dialogue_item.message = "Hey you! Come over here!"
+	#dialogue_item.message_index = 1
+	#dialogue_item.add_dialogue_option("Okay, what do you want?", 3)
+	#dialogue_item.add_dialogue_option("No.", 2)
+	#dialogue_items.append(dialogue_item)
+	#for item in dialogue_items:
+		#print(item.message)
+		#print(item.options)
+	#_save()
+	pass
+	
+	
+## saves the dialogue in the proper format, no longer needed
+func _save() -> void :
+	var file = FileAccess.open(DIALOGUE_FILE_PATH, FileAccess.WRITE)
+	var save_actor_dialogue = actor_name.to_lower() + "_" + "dialogue_items"
+	var save_dict = {
+		"actor_name" : actor_name.to_lower(),
+		save_actor_dialogue : []
+	}
 	for item in dialogue_items:
-		print(item.message)
-		print(item.choices)
-
+		var new_dict = {
+			"message_index" : item.message_index,
+			"message" : item.message,
+			"options" : item.options,
+		}
+		save_dict[save_actor_dialogue].append(new_dict)
+	file.store_line(JSON.stringify(save_dict, "\t"))
+	file.close()
+	
 
 
 func _load_dialogue_items() -> void :
@@ -29,5 +60,18 @@ func _load_dialogue_items() -> void :
 
 
 class DialogueItem:
-	var message : String = ""
-	var choices : Array[String] = []
+	const OPTION_TEXT = "option_text"
+	const GOTO_MESSAGE_INDEX = "goto_message_index"
+	var message :String = ""
+	var message_index :int = 0
+	var options :Array[Dictionary] = []
+	
+	func add_dialogue_option(new_option_text:String, new_goto_message_index:int = -1):
+		if new_goto_message_index > 0: new_goto_message_index -= 1
+		var dialogue_option = {
+			OPTION_TEXT : new_option_text,
+			GOTO_MESSAGE_INDEX : new_goto_message_index,
+		}
+		options.append(dialogue_option)
+
+		
