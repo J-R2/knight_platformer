634ca6c747bb801c99a973241fbbb6d9cc307ed2
Jordan <104177490+J-R2@users.noreply.github.com>
2024-10-21 14:31:37 -0500

    improved passive_dialogue_cue_area

--- systems/dialogue/passive_dialogue_cue_area/passive_dialogue_cue_area.gd
+++ systems/dialogue/passive_dialogue_cue_area/passive_dialogue_cue_area.gd
@@ -1,33 +1,25 @@
 class_name PassiveDialogueCueArea
 extends Area2D
 
-
-@onready var control: Control = $CanvasLayer/Control
-@onready var dialogue_bubble_sprite: Sprite2D = $CanvasLayer/Control/DialogueBubbleSprite
 @export var message = ""
-@onready var rich_text_label: RichTextLabel = $CanvasLayer/Control/DialogueBubbleSprite/RichTextLabel
-
-
-const TOP_LEFT = Vector2(60, 60)
-const TOP_RIGHT = Vector2(580, 60)
-const BOTTOM_LEFT = Vector2(60, 200)
-const BOTTOM_RIGHT = Vector2(580, 200)
-const POSITIONS = [TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT]
-@export_enum("TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT") var dialogue_bubble_position = 0
 @export_range(1.0, 5.0) var show_timer_length = 3.0
+@onready var collisions :Array[CollisionShape2D] = [$CollisionShape2D, $CollisionShape2D2, $CollisionShape2D3, $CollisionShape2D4]
+@onready var label: Label = $DialogueBubble/Label
+@onready var dialogue_bubble: Sprite2D = $DialogueBubble
+
+
 
 func _ready() -> void:
-	dialogue_bubble_sprite.position = POSITIONS[dialogue_bubble_position]
-	rich_text_label.text = message
-	control.hide()
+	label.text = message
+	dialogue_bubble.hide()
 	self.body_entered.connect(_on_body_entered)
 	
 	
 	
 func _on_body_entered(body:Node2D) -> void :
-	control.show()
-	var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-	collision_shape_2d.disabled = true
+	dialogue_bubble.show()
+	for collision in collisions:
+		collision.disabled = true
 	var timer = Timer.new()
 	timer.wait_time = show_timer_length
 	timer.one_shot = true
@@ -37,9 +29,8 @@ func _on_body_entered(body:Node2D) -> void :
 
 
 func _kill_self() -> void :
-	print("died")
 	var tween = create_tween()
 	var duration :float = 0.25
-	tween.tween_property(dialogue_bubble_sprite, "scale", Vector2.ZERO, duration)
+	tween.tween_property(dialogue_bubble, "scale", Vector2.ZERO, duration)
 	await tween.finished
 	self.queue_free()
